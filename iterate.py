178bb454780cf055b4c5d534ca8d0a4e49c95e86
Zotman03 <zhenglin.ning@outlook.com>
2025-11-17 22:06:56 -0500

    gitignore and unrelated files ignored

--- iterate.py
+++ iterate.py
@@ -9,6 +9,7 @@ def generate_dataset(num_pages, out_dir):
         img_path = f"{out_dir}/images/page_{i:05d}.png"
         json_path = f"{out_dir}/labels/page_{i:05d}.json"
         generate_page(img_path, json_path)
+        print(f"{i + 1} documents generated")
 
 generate_dataset(8000, "synthetic_dataset/train")
 generate_dataset(1000, "synthetic_dataset/val")
